a62848e2be5c04d5d911623f2ce79857af96440d
Tomás Centurión <t.centurion@educacionit.com>
2023-02-01 06:15:35 -0600

    Agrego practica

---
+++ SLNClinica/SLNClinica/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace SLNClinica.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<SLNClinica.Data.DBProductosBootcampContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(SLNClinica.Data.DBProductosBootcampContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
